e8c5fcc183e2857c2bf9b9553309ee49dcfe4d61
max <777max8@gmail.com>
2022-07-06 14:54:07 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimalsComponent } from './animals/animals.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'animals', component: AnimalsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
